fd3e0df243e3722ad584a9c693b36f0cf5fe1231
Max <zeetreex@gmail.com>
2022-05-10 20:48:49 +0300

    endpoint with regexp, any data via * sign

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,9 @@ const PORT = 3000
 app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 
-app.route('/')
-  .get((req: Request, res: Response) => {
-    return res.send("You made a GET request")
-  })
-  .post((req: Request, res: Response) => {
-    return res.send("You made a POST request")
-  })
+app.get('/health', (req: Request, res: Response) => res.sendStatus(200))
+app.get('/ab*cd', (req: Request, res: Response) => res.send("ab(your data)cd"))
+app.get(/abc/, (req: Request, res: Response) => res.send("abc"))
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:', PORT)
