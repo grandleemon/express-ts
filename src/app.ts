2d8de3845cb7ec5b61bcc2b47d993ce18135872e
Max <zeetreex@gmail.com>
2022-05-10 20:41:03 +0300

    created a route for different types of request for one endpoint

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,13 @@ const PORT = 3000
 app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 
-app.get("/", (req: Request, res: Response) => {
-  return res.redirect("http://example.com")
-})
-
-app.post('/api/data', (req: Request, res: Response) => {
-  console.log(req.body)
-
-  return res.sendStatus(200)
-})
-
-app.all('/api/all', (req: Request, res: Response) => {
-  return res.sendStatus(200)
-})
+app.route('/')
+  .get((req: Request, res: Response) => {
+    return res.send("You made a GET request")
+  })
+  .post((req: Request, res: Response) => {
+    return res.send("You made a POST request")
+  })
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:', PORT)
