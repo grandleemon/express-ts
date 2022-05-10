20237661730443bac9125cbc9c7fce0305d63875
Max <zeetreex@gmail.com>
2022-05-10 20:34:24 +0300

    wrote a few endpoint handlers, typed the request and response arguments in the callback function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log('Hello world')
+import express, {Request, Response} from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req: Request, res: Response) => {
+  return res.redirect("http://example.com")
+})
+
+app.post('/api/data', (req: Request, res: Response) => {
+  console.log(req.body)
+
+  return res.sendStatus(200)
+})
+
+app.all('/api/all', (req: Request, res: Response) => {
+  return res.sendStatus(200)
+})
+
+app.listen(PORT, () => {
+  console.log('Server started at http://localhost:', PORT)
+})
